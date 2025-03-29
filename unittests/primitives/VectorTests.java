14bb5ff4ed01f0674cde183fc60a33803941ad8b
Yedidia Bakuradze <yedidiadev@gmail.com>
2025-03-29 23:07:41 +0300

    Cross-product test has been implemented

--- unittests/primitives/VectorTests.java
+++ unittests/primitives/VectorTests.java
@@ -1,7 +1,8 @@
 package primitives;
 
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.fail;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * Unit tests for {@link primitives.Vector}.
@@ -97,13 +98,47 @@ class VectorTests {
      */
     @Test
     void testCrossProduct() {
+        float DELTA = 0.00001f;
         // ============ Equivalence Partitions Tests ==============
+
+        //TC01: test that length of the cross-product of vectors with less than 90 degrees angle (45d) is correct
+        Vector vec1 = new Vector(1,0,0);
+        Vector vec2 = new Vector(1,0,-1);
+        Vector vr = vec1.crossProduct(vec2);
+        assertEquals(vec1.length()*vec2.length()*Math.sin(45), vr.length(), DELTA, "ERROR: crossProduct() wrong result length");
+
+        //TC02: test that length of the cross-product of vectors with a degree higher than 90 degrees angle is correct
+        vec1 = new Vector(1,0,1);
+        vec2 = new Vector(-1,0,-1);
+        vr = vec1.crossProduct(vec2);
+        assertEquals(vec1.length()*vec2.length()*Math.sin(135), vr.length(), DELTA, "ERROR: crossProduct() wrong result length");
+
+        //TC02: test that length of the cross-product of vectors with a degree equally 90 degrees angle is correct
+        vec1 = new Vector(1,0,0);
+        vec2 = new Vector(1,1,0);
+        vr = vec1.crossProduct(vec2);
+        assertEquals(vec1.length()*vec2.length()*1, vr.length(), DELTA, "ERROR: crossProduct() wrong result length");
 
 
         // =============== Boundary Values Tests ==================
 
+        //TC11: test that a cross-product of parallel vectors throws an exception
+        assertThrows(IllegalArgumentException.class ,()->new Vector(1,0,0).crossProduct(new Vector(3,0,0)), "ERROR: crossProduct() for parallel vectors does not throw an exception");
 
-        fail("No test implementation in Vector:testCrossProduct");
+        //TC12: test that a cross-product of negating vectors with the same size equals to zero
+        Vector vec3 = new Vector(1,2,3);
+        Vector vec4 = new Vector(-1,-2,-3);
+        assertEquals(Vector.ZERO,vec3.crossProduct(vec4), "ERROR: crossProduct() for negating vectors with same sizes is not a zero vector");
+
+        //TC13: test that a cross-product of negating vectors with the different sizes equals to zero
+        vec3 = new Vector(7,0,0);
+        vec4 = new Vector(-5,0,0);
+        assertEquals(Vector.ZERO,vec3.crossProduct(vec4), "ERROR: crossProduct() for negating vectors with different sizes is not a zero vector");
+
+        //TC14: test that a cross-product of negating vectors with the different sizes equals to zero
+        vec3 = new Vector(7,7,0);
+        vec4 = new Vector(7,7,0);
+        assertEquals(Vector.ZERO,vec3.crossProduct(vec4), "ERROR: crossProduct() for vectors with same sizes and same direction is not a zero vector");
     }
 
     /**
