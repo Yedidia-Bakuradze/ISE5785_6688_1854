b394f29abe55c274fd12880624daef676a5962da
Yedidia Bakuradze <yedidiadev@gmail.com>
2025-04-03 12:51:35 +0300

    Sphere tests been noted

--- unittests/geometries/SphereTests.java
+++ unittests/geometries/SphereTests.java
@@ -43,6 +43,50 @@ public class SphereTests {
      */
     @Test
     void testFindIntersections() {
-        fail("Test not implemented yet");
+        // ============ Equivalence Partitions Tests ==============
+
+        // TC01: Ray that starts before the sphere and intersects it twice
+
+        // TC02: Ray that starts inside the sphere and intersects it once
+
+        // TC03: Ray that starts outside the sphere and the ray goes exactly in the opposite direction of the object
+
+        // TC04: Ray that starts outside the sphere and the ray line doesn't intersect the object on any side
+
+        // =============== Boundary Values Tests ==================
+
+        // *** Group 1: Ray is tangent to the sphere ***
+        // TC11: Ray that starts before the sphere and is tangent to it
+
+        // TC12: Ray that starts from the tangent point
+
+        // TC13: Ray that starts after the sphere and is tangent to it
+
+
+        // *** Group 2: Ray's starting point is orthogonal to line between it and the center of the sphere ***
+
+        // TC14: Ray's starting point is outside the sphere
+
+        // TC15: Ray's starting point is on the sphere
+
+
+        // *** Group 3: Ray's line crosses the sphere twice ***
+        // TC16: Ray's starting point is on the sphere and goes into the sphere
+
+        // TC17: Ray's starting point is on the sphere and goes out of the sphere
+
+
+        // *** Group 4: Ray's line crosses the center of the sphere ***
+        // TC18: Ray's starting point is outside the sphere and goes out of the sphere
+
+        // TC19: Ray's starting point is the center of the sphere
+
+        // TC20: Ray's starting point is on the sphere and goes out of the sphere
+
+        // TC21: Ray's starting point is on the sphere and goes inside the sphere
+
+        // TC22: Ray's starting point is inside the sphere and goes out of the sphere
+
+        // TC23: Ray's starting point is inside the sphere and goes inside the sphere
     }
 }
