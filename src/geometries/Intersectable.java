9a5b55581e81e15c11fa1bc074b39e82f957e330
Yedidia Bakuradze <yedidiadev@gmail.com>
2025-04-04 16:11:24 +0300

    Intersectable interface has been added

---
+++ src/geometries/Intersectable.java
@@ -0,0 +1,18 @@
+package geometries;
+
+import primitives.Point;
+import primitives.Ray;
+import java.util.List;
+
+/**
+ * Interface for geometries that can be intersected by rays.
+ */
+public interface Intersectable {
+
+    /**
+     * Finds the ray intersections with the geometry.
+     * @param ray The cast ray.
+     * @return A list of intersection points (LinkedList instance), or null value if there are no intersections.
+     */
+    List<Point> findIntsersections(Ray ray);
+}
