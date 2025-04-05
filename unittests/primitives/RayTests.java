9a6c96502ad778e0c7576b39215a03e6853da28d
Yedidia Bakuradze <yedidiadev@gmail.com>
2025-04-06 00:24:53 +0300

    testGetPoint test method at RayTests' class has been added with a default implementation

---
+++ unittests/primitives/RayTests.java
@@ -0,0 +1,16 @@
+package primitives;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RayTests {
+
+    /**
+     * Test method for {@link Ray#getPoint(double)}.
+     */
+    @Test
+    void testGetPoint() {
+        fail("Not implemented yet");
+    }
+}
