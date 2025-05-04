8230e943eb6bfbc2c519ed881154d08051bb4081
Yedidia Bakuradze <yedidiadev@gmail.com>
2025-05-04 14:59:31 +0300

    SimpleRayTracer class has been implemented in very basic way

---
+++ src/renderer/SimpleRayTracer.java
@@ -0,0 +1,17 @@
+package renderer;
+
+import primitives.Color;
+import primitives.Ray;
+import scene.Scene;
+
+public class SimpleRayTracer extends RayTracerBase {
+
+    SimpleRayTracer(Scene scene) {
+        super(scene);
+    }
+
+    @Override
+    public Color traceRay(Ray ray) {
+        throw new UnsupportedOperationException("Tray method has been implemented yet in the son's class");
+    }
+}
