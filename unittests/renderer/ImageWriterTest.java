2c51dec031fcc92133f9effac09235f81cdf2062
Yedidia Bakuradze <yedidiadev@gmail.com>
2025-05-04 14:15:08 +0300

    Main window generator test has been defined (not implemented yet)

---
+++ unittests/renderer/ImageWriterTest.java
@@ -0,0 +1,19 @@
+package renderer;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class ImageWriterTest {
+
+    /*
+    * This test made for testing a build of a main window
+    * It's ViewPlane: 10X16 squares
+    * It's regulation: 800X500
+    * Background color: TODO Need to be defined
+     */
+    @Test
+    void testBuildMainImage(){
+        fail("Not yet implemented");
+    }
+}
