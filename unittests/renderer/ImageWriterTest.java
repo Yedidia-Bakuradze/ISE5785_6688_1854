a72d8ef4f0c403ffaa649301766d542a6f0d1c5e
Yedidia Bakuradze <yedidiadev@gmail.com>
2025-05-04 14:36:25 +0300

    The test for creating first main image has been implemented and passed

--- unittests/renderer/ImageWriterTest.java
+++ unittests/renderer/ImageWriterTest.java
@@ -1,7 +1,9 @@
 package renderer;
 
 import org.junit.jupiter.api.Test;
+import primitives.Color;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.fail;
 
 public class ImageWriterTest {
@@ -10,10 +12,20 @@ public class ImageWriterTest {
     * This test made for testing a build of a main window
     * It's ViewPlane: 10X16 squares
     * It's regulation: 800X500
-    * Background color: TODO Need to be defined
+    * Background color: Red for bordering, Yellow for actual main color
      */
     @Test
     void testBuildMainImage(){
-        fail("Not yet implemented");
+        assertDoesNotThrow(() -> {
+            Color mainColor = new Color(java.awt.Color.YELLOW);
+            Color borderColor = new Color(java.awt.Color.RED);
+            ImageWriter imageWriter = new ImageWriter(800, 500);
+            for (int i = 0; i < 800; i++) {
+                for (int j = 0; j < 500; j++) {
+                    imageWriter.writePixel(i, j, i % 50 == 0 || j % 50 == 0 ? borderColor : mainColor);
+                }
+            }
+            imageWriter.writeToImage("firstImage");
+        }, "Failed to create image");
     }
 }
