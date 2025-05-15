c1aa75c7afe6714a6a4641c37c96c3313c9d70e0
Yedidia Bakuradze <yedidiadev@gmail.com>
2025-05-15 18:14:36 +0300

    (Fixed) Intersection class has been implemented

--- src/geometries/Intersectable.java
+++ src/geometries/Intersectable.java
@@ -10,7 +10,7 @@ import java.util.List;
  */
 public abstract class Intersectable {
 
-    public static class g {
+    public static class Intersection {
         public final Geometry geometry;
         public final Point point;
 
