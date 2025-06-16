857b68aea5b5d599c67a8ac738f7ab7abdffff18
Yedidia Bakuradze <yedidiadev@gmail.com>
2025-06-16 12:58:07 +0300

    SuperSamplingMode enum has been updated

--- src/sampling/SuperSamplingMode.java
+++ src/sampling/SuperSamplingMode.java
@@ -1,7 +1,15 @@
 package sampling;
 
 public enum SuperSamplingMode {
-    EASY,
-    DEMO,
-    PRODUCTION
+    EASY(2),
+    DEMO(3),
+    PRODUCTION(4);
+
+    public final int gridSize;
+    public final int gridSizeSquared;
+
+    SuperSamplingMode(int gridSize) {
+        this.gridSize = gridSize;
+        this.gridSizeSquared = gridSize * gridSize;
+    }
 }
