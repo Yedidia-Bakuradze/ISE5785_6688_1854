7fbacdebe812773c0949e3c33ecb07df1cfabfd5
Yedidia Bakuradze <yedidiadev@gmail.com>
2025-06-19 10:22:13 +0300

    Effect type enum has been added. One parameter for each type of feature

---
+++ src/renderer/EffectType.java
@@ -0,0 +1,10 @@
+package renderer;
+
+// Enum for different effect types
+public enum EffectType {
+    DIFFUSIVE_GLASS,
+    SOFT_SHADOW,
+    ANTI_ALIASING,
+    GLOSSY_REFLECTION,
+    DEPTH_OF_FIELD
+}
