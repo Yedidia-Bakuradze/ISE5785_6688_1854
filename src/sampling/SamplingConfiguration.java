fb42decc22eca6219b1a83e7a42881db1d00aead
Yedidia Bakuradze <yedidiadev@gmail.com>
2025-06-19 10:35:27 +0300

    Fields of the sample config class switched to public

--- src/sampling/SamplingConfiguration.java
+++ src/sampling/SamplingConfiguration.java
@@ -3,11 +3,11 @@ package sampling;
 import java.util.Random;
 
 public class SamplingConfiguration {
-    private final SamplingMode mode;
-    private final TargetAreaType shape;
-    private final SamplingPattern pattern;
-    private final double radius;
-    private final Random random;
+    public final SamplingMode mode;
+    public final TargetAreaType shape;
+    public final SamplingPattern pattern;
+    public final double radius;
+    public final Random random;
 
     public SamplingConfiguration(SamplingMode mode, TargetAreaType shape, SamplingPattern pattern, double radius) {
         this.mode = mode;
