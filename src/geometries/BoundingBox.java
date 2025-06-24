c7edeb791193eb19ad01e5d4208e0248644a73a9
Yedidia Bakuradze <yedidiadev@gmail.com>
2025-06-24 16:34:31 +0300

    Intersection logic of the voxels has been moved to BooudingBox

--- src/geometries/BoundingBox.java
+++ src/geometries/BoundingBox.java
@@ -1,6 +1,9 @@
 package geometries;
 
-import primitives.Point;
+import primitives.*;
+
+import java.util.Collection;
+import java.util.Objects;
 
 /**
  * Represents an Axis-Aligned Bounding Box in 3D space.
@@ -41,6 +44,21 @@ public record BoundingBox(Point min, Point max) {
     }
 
     /**
+     * Creates a union of multiple bounding boxes
+     *
+     * @param boxes Collection of bounding boxes to unite
+     * @return A new bounding box that contains all input boxes, or null if collection is empty or all boxes are null
+     */
+    public static BoundingBox union(Collection<BoundingBox> boxes) {
+        return boxes == null || boxes.isEmpty()
+                ? null
+                : boxes.stream()
+                .filter(Objects::nonNull)
+                .reduce(BoundingBox::expand)
+                .orElse(null);
+    }
+
+    /**
      * Expands this AABB to include another AABB
      *
      * @param other The other AABB to include
@@ -56,6 +74,93 @@ public record BoundingBox(Point min, Point max) {
         double maxZ = Math.max(this.max.getZ(), other.max.getZ());
 
         return new BoundingBox(new Point(minX, minY, minZ), new Point(maxX, maxY, maxZ));
+    }
+
+    /**
+     * Ray-box intersection test using slab method
+     *
+     * @param ray The ray to test
+     * @return true if ray intersects this box, false otherwise
+     */
+    public boolean intersects(Ray ray) {
+        Point rayOrigin = ray.getHead();
+        Vector rayDir = ray.getDirection();
+
+        double tMin = Double.NEGATIVE_INFINITY;
+        double tMax = Double.POSITIVE_INFINITY;
+
+        // Check intersection with each pair of parallel planes
+        double[] origins = {rayOrigin.getX(), rayOrigin.getY(), rayOrigin.getZ()};
+        double[] directions = {rayDir.getX(), rayDir.getY(), rayDir.getZ()};
+        double[] mins = {min.getX(), min.getY(), min.getZ()};
+        double[] maxs = {max.getX(), max.getY(), max.getZ()};
+
+        for (int i = 0; i < 3; i++) {
+            if (Math.abs(directions[i]) < 1e-10) {
+                // Ray is parallel to slab
+                if (origins[i] < mins[i] || origins[i] > maxs[i]) {
+                    return false;
+                }
+            } else {
+                double t1 = (mins[i] - origins[i]) / directions[i];
+                double t2 = (maxs[i] - origins[i]) / directions[i];
+
+                if (t1 > t2) {
+                    double temp = t1;
+                    t1 = t2;
+                    t2 = temp;
+                }
+
+                tMin = Math.max(tMin, t1);
+                tMax = Math.min(tMax, t2);
+
+                if (tMin > tMax) {
+                    return false;
+                }
+            }
+        }
+
+        return tMax >= 0;
+    }
+
+    /**
+     * Calculates the entry point where a ray first intersects this bounding box
+     *
+     * @param ray The ray to test
+     * @return The entry point, or null if no intersection
+     */
+    public Point getRayEntryPoint(Ray ray) {
+        if (!intersects(ray)) return null;
+
+        Point rayOrigin = ray.getHead();
+        Vector rayDir = ray.getDirection();
+
+        // If ray starts inside the box, return the origin
+        if (contains(rayOrigin)) return rayOrigin;
+
+        // Find the entry point using slab method
+        double tEntry = Double.NEGATIVE_INFINITY;
+
+        double[] origins = {rayOrigin.getX(), rayOrigin.getY(), rayOrigin.getZ()};
+        double[] directions = {rayDir.getX(), rayDir.getY(), rayDir.getZ()};
+        double[] mins = {min.getX(), min.getY(), min.getZ()};
+        double[] maxs = {max.getX(), max.getY(), max.getZ()};
+
+        for (int i = 0; i < 3; i++) {
+            if (Math.abs(directions[i]) > 1e-10) {
+                double t1 = (mins[i] - origins[i]) / directions[i];
+                double t2 = (maxs[i] - origins[i]) / directions[i];
+
+                double tNear = Math.min(t1, t2);
+                if (tNear > tEntry) {
+                    tEntry = tNear;
+                }
+            }
+        }
+
+        return tEntry < 0
+                ? null
+                : rayOrigin.add(rayDir.scale(tEntry));
     }
 
     /**
